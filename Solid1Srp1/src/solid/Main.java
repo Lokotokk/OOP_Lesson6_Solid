be2ec3c76cc3e304663f8f3fa2274191c8de0744
olya <abc@mail.ru>
2023-06-16 18:38:11 +0300

    переделано задание 2 в соотв. с семинаром

--- Solid1Srp1/src/solid/Main.java
+++ Solid1Srp1/src/solid/Main.java
@@ -2,15 +2,18 @@ package solid;
 
 import solid.srp.Point;
 import solid.srp.Square;
+import solid.srp.SquareDraw;
 
 public class Main {
     public static void main(String[] args) {
         int side = 5;
-        Square square = new Square(side);
+        Square square = new Square(new Point(1,1), side);
+        SquareDraw squareDraw = new SquareDraw(side);
+        squareDraw.draw();
         System.out.printf("Площадь фигуры: %d \n", square.getArea());
-        square.draw();
-        square.zoom(1.5f);
+        
+        squareDraw.setSide(square.getSide() * 2);
+        squareDraw.draw();
         System.out.printf("\nПлощадь фигуры: %d \n", square.getArea());
-        square.draw();
     }
 }
--- Solid1Srp1/src/solid/srp/Square.java
+++ Solid1Srp1/src/solid/srp/Square.java
@@ -1,35 +1,40 @@
 package solid.srp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Square {
 
+    private List<Point> points;
     private int side;
 
-    public Square(int side) {
+    public Square(Point point1, int side) {
         this.side = side;
+
+        Point point2 = new Point(point1.getX() + side, point1.getY());
+        Point point3 = new Point(point1.getX() + side, point1.getY() + side);
+        Point point4 = new Point(point1.getX(), point1.getY() + side);
+
+        points = new ArrayList<>(4);
+        points.add(point1);
+        points.add(point2);
+        points.add(point3);
+        points.add(point4);
+    }
+
+    public List<Point> getPoints() {
+        return points;
     }
 
     public int getArea() {
         return side * side;
     }
 
-    public void zoom(float zoom) {
-        side = (int) (side * zoom);
+    public void setSide(int side) {
+        this.side = side;
     }
 
-    public void draw() {
-        for (int i = 0; i < side; i++) {
-            System.out.print("* ");
-        }
-        System.out.println();
-        for (int i = 0; i < side-2; i++) {
-            System.out.print("* ");
-            for (int j = 1; j < side - 1; j++) {
-                System.out.print("  ");
-            }
-            System.out.println("*");
-        }
-        for (int i = 0; i < side; i++) {
-            System.out.print("* ");
-        }
+    public int getSide() {
+        return side;
     }
 }
