954602a8dc6cccd7bcf3e4bc01684a9a595447af
olya <abc@mail.ru>
2023-06-19 09:54:54 +0300

    Поправлено задание 6

--- Solid6Isp1/src/solid/Main.java
+++ Solid6Isp1/src/solid/Main.java
@@ -2,9 +2,10 @@ package solid;
 
 import solid.isp.InternetPaymentService;
 import solid.isp.TerminalPaymentService;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotImplementedException {
         InternetPaymentService internetService = new InternetPaymentService();
         internetService.payWebMoney(10);
         internetService.payCreditCard(10);
@@ -13,6 +14,5 @@ public class Main {
         TerminalPaymentService terminalService = new TerminalPaymentService();
         terminalService.payWebMoney(10);
         terminalService.payCreditCard(10);
-        terminalService.payPhoneNumber(10);
     }
 }
---
+++ Solid6Isp1/src/solid/isp/PayablePhoneNumber.java
@@ -0,0 +1,7 @@
+package solid.isp;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public interface PayablePhoneNumber {
+    void payPhoneNumber(int amount) throws NotImplementedException;
+}
