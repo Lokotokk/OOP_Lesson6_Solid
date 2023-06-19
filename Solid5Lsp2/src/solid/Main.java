ba2c88e6765bed315c09eb37e0431835c0187bae
olya <abc@mail.ru>
2023-06-19 08:53:51 +0300

    поправлено задание 5

--- Solid5Lsp2/src/solid/Main.java
+++ Solid5Lsp2/src/solid/Main.java
@@ -2,16 +2,16 @@ package solid;
 
 import solid.lsp.FactoryOrder;
 import solid.lsp.Order;
+import solid.lsp.OrderAbstract;
 
 public class Main {
     public static void main(String[] args) {
         FactoryOrder creator = new FactoryOrder();
         OrderCalculator calculator = new solid.OrderCalculator();
-        calculator.add(creator.create(3, 2));
-        calculator.add(creator.create(1, 3));
-        calculator.add(creator.create(2, 2));
+        calculator.add(creator.create(3, 2, false));
+        calculator.add(creator.create(1, 3, true));
 
-        for (Order order : calculator) {
+        for (OrderAbstract order : calculator) {
             System.out.println(order);
         }
 
